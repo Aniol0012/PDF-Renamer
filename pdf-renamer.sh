e3ad1b16773d7670be6c73af4ba7d0fae029aad3
Aniol Serrano <53788631+Aniol0012@users.noreply.github.com>
2023-01-24 08:51:07 +0100

    Add files via upload

--- pdf-renamer.sh
+++ pdf-renamer.sh
@@ -12,6 +12,7 @@ print_line() {
 print_star() {
     echo -n "[*] "
 }
+
 # Auxiliar functions
 
 folder_name="PDF-Backup"
@@ -21,7 +22,7 @@ help_panel() {
     echo -e -n "\nThis program helps you to rename pdfs"
     echo -e ", by default will do it on the current path\n"
     echo -e "List of parameters:\n"
-    echo -e "\t-h: Show this help panel"
+    echo -e "\t-h: Shows this help panel"
     echo -e "\t-c: Creates the specified number of pdf files"
     echo -e "\t-a: Deletes all the pdf files in the current directory"
     echo -e "\t-b: Make backup of the files in the folder named PDF-Backup"
@@ -32,6 +33,8 @@ help_panel() {
     print_line
     exit 0
 }
+
+# Checks if you have all the dependencies you need, if not, it installs it
 
 dependencies() {
     clear
@@ -57,98 +60,7 @@ dependencies() {
     echo ""
 }
 
-remove_file() {
-    for file in *.pdf; do
-        if [[ $file == "*.pdf" ]]; then # In case there are no pdf files the $file is equal as in the for loop
-            print_line
-            print_star
-            echo "There are no pdf files to remove"
-            print_line
-            exit 1
-        else
-            print_line
-            rm -rf *.pdf
-            find . -type f ! -name "*.*" -exec rm -f {} \; # Removes all the files that dont have file extension
-            print_star
-            echo -e "All pdf tests have been removed"
-            print_line
-            exit 0
-        fi
-    done
-}
-
-list_pdfs() {
-    print_line
-    if ls -1 *.pdf >/dev/null 2>&1; then
-        print_star
-        echo "The list of pdfs is:"
-        echo -e "$(ls -1 *.pdf)"
-        print_line
-    else
-        print_star
-        echo "There are no pdf files to list"
-        print_line
-        exit 1
-    fi
-    exit 0
-}
-
-delete_backup() {
-    rm -rf $folder_name
-    if [[ $1 == "-f" ]]; then
-        print_line
-        print_star
-        echo "Folder named $folder_name has been removed"
-        print_line
-        exit 0
-    fi
-}
-
-make_backup() {
-    print_line
-    delete_backup
-    mkdir $folder_name
-    for file in *.pdf; do
-        cp -f $file $folder_name
-    done
-    print_star
-    echo "The backup has been created in $folder_name"
-}
-
-rename_file_final() {
-    let i+=1
-    mv $file ${filename}_$i.pdf
-    echo -e "\t\t- $file has been renamed to ${filename}_$i.pdf"
-}
-
-rename_file() {
-    print_line
-    # By now they are not needed
-    #dependencies
-    print_star
-    let i=0
-    echo "Renaming all .pdfs on current path [$(pwd)]:"
-    sleep 1
-    if ls -1 *.pdf >/dev/null 2>&1; then # Doesent work the other way
-        echo -n ""
-    else
-        print_star
-        echo "There are no pdf files to rename"
-        print_line
-        exit 1
-    fi
-
-    echo -e -n "\t"
-    print_star
-    read -p "Give a filename: " filename
-    for file in *.pdf; do
-        rename_file_final $i
-    done
-    print_star
-    echo "All files renamed"
-    sleep 1
-    print_line
-}
+# Creates the amount of pdf files are required
 
 create_file() {
     print_line
@@ -180,6 +92,112 @@ create_file() {
     fi
     print_line
     exit 0
+}
+
+# Removes all the pdf files in the current directory
+
+remove_file() {
+    for file in *.pdf; do
+        if [[ $file == "*.pdf" ]]; then # In case there are no pdf files the $file is equal as in the for loop
+            print_line
+            print_star
+            echo "There are no pdf files to remove"
+            print_line
+            exit 1
+        else
+            print_line
+            rm -rf *.pdf
+            find . -type f ! -name "*.*" -exec rm -f {} \; # Removes all the files that dont have file extension
+            print_star
+            echo -e "All pdf tests have been removed"
+            print_line
+            exit 0
+        fi
+    done
+}
+
+# Lists all the pdf files, same as ls *.pdf
+
+list_pdfs() {
+    print_line
+    if ls -1 *.pdf >/dev/null 2>&1; then
+        print_star
+        echo "The list of pdfs is:"
+        echo -e "$(ls -1 *.pdf)"
+        print_line
+    else
+        print_star
+        echo "There are no pdf files to list"
+        print_line
+        exit 1
+    fi
+    exit 0
+}
+
+# Deletes the backup of files
+
+delete_backup() {
+    rm -rf $folder_name
+    if [[ $1 == "-f" ]]; then
+        print_line
+        print_star
+        echo "Folder named $folder_name has been removed"
+        print_line
+        exit 0
+    fi
+}
+
+# Makes a simple security backup just in case
+
+make_backup() {
+    print_line
+    delete_backup
+    mkdir $folder_name
+    for file in *.pdf; do
+        cp -f $file $folder_name
+    done
+    print_star
+    echo "The backup has been created in $folder_name"
+}
+
+# Renaming files by the given name
+
+rename_file_final() {
+    let i+=1
+    mv $file ${filename}_$i.pdf
+    echo -e "\t\t- $file has been renamed to ${filename}_$i.pdf"
+}
+
+rename_file() {
+    print_line
+    print_star
+    let i=0
+    echo "Renaming all .pdfs on current path [$(pwd)]:"
+    sleep 1
+    if ls -1 *.pdf >/dev/null 2>&1; then # Doesent work the other way
+        echo -n ""
+    else
+        print_star
+        echo "There are no pdf files to rename"
+        print_line
+        print_star
+        read -p "Do you want to create one? [y/n]: " answer
+        if [[ $answer == "yes" || $answer == "y" ]]; then
+            create_file
+        else
+            print_line
+            exit 1
+        fi
+    fi
+    echo -e -n "\t"
+    print_star
+    read -p "Give a filename: " filename
+    for file in *.pdf; do
+        rename_file_final $i
+    done
+    print_star
+    echo "All files renamed"
+    print_line
 }
 
 # Main function:
